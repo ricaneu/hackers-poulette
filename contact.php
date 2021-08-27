537bdecb7c976c56fc5402e5e976612d5b520f04
ricaneu <79845716+ricaneu@users.noreply.github.com>
2021-08-27 20:04:17 +0200

    rebase

--- contact.php
+++ contact.php
@@ -1,14 +1,53 @@
 <?php
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL); // to show error but doesn't work
 $name = $_POST['name'];
 $gender = $_POST['gender'];
 $email = $_POST['email'];
 $country = $_POST['country'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Email: $email \n Subject: $subject \n Message: $message";
-$recipient = "ricaneu@gmail.COM";
-$subject = "subject";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You for your message! We will contact you as soon as possible" . " -" . "<a href='form.html' style='text-decoration:none;color:#0d8187;'> Return Home</a>";
+$error = [] ;
+
+///var_dump()- use that to test the value of the variable
+
+if (empty($name)) {    
+    $error[] = 'please write your name';
+}
+
+if (empty($gender)) {    
+    $error[] = 'please write your gender';
+}
+
+if (empty($email)) {    
+    $error[] = 'please write your email';
+}
+
+if (empty($country)) {    
+    $error[] = 'please write your country';
+}
+
+if (empty($message)) {    
+    $error[] = 'please write your message';
+}
+
+
+if (!empty($error)) {
+    print_r ($error);
+    $a = "<a href='form.html' style='text-decoration:none;color:#0d8187;'> Return Home</a>";
+    print_r ($a);
+}else {
+    $formcontent=htmlspecialchars(" From: $name \n Email: $email \n Subject: $subject \n Message: $message");
+    // htmlspecialchars is for sanitizing
+    $recipient = "ricaneu@gmail.COM";
+    $subject = "subject";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    $a = "Thank You for your message! We will contact you as soon as possible" . " -" . "<a href='form.html' style='text-decoration:none;color:#0d8187;'> Return Home</a>";
+    print_r ($a);
+}
+
+
+
+
 ?>
